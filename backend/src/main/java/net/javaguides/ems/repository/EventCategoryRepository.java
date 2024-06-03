32c422b6212b8d305f436e88c03c1ad9f481d583
Aleksandra Gabka <aleksandraxgabka@gmail.com>
2024-06-03 02:00:49 +0200

    added admin role and possibility to add events from the admin account

---
+++ backend/src/main/java/net/javaguides/ems/repository/EventCategoryRepository.java
@@ -0,0 +1,7 @@
+package net.javaguides.ems.repository;
+
+import net.javaguides.ems.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository, Long> {
+}
